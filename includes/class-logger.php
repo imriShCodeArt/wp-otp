16d2074f0800ead9d79a96a2d91c6dcd682a44dc
imriSh <59433290+imriShCodeArt@users.noreply.github.com>
2025-07-04 13:33:12 +0300

    feat: initialized logger class

--- includes/class-logger.php
+++ includes/class-logger.php
@@ -9,8 +9,32 @@ if (!defined('ABSPATH')) {
 class WP_OTP_Logger
 {
 
-    public function log($event_type, $phone, $message)
+    protected $table_name;
+
+    public function __construct()
     {
-        // Log to DB or file
+        global $wpdb;
+        $this->table_name = $wpdb->prefix . 'otp_logs';
+    }
+
+    public function log($event_type, $contact, $message)
+    {
+        global $wpdb;
+
+        $wpdb->insert(
+            $this->table_name,
+            [
+                'event_type' => $event_type,
+                'contact' => $contact,
+                'message' => $message,
+                'created_at' => current_time('mysql', 1),
+            ],
+            [
+                '%s',
+                '%s',
+                '%s',
+                '%s',
+            ]
+        );
     }
 }
